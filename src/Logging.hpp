66956154f394db1d19cc2d1d9c0d2b316d892417
PickGG (Kirill Andreev) <issueswq@gmail.com>
2026-01-24 01:42:21 +0500

    Started developing SDL3+Opengl3. Added starter program

--- src/Logging.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <SDL3/SDL_log.h>
-
-#define LOG_TRACE(fmt, ...) SDL_LogTrace(SDL_LOG_CATEGORY_APPLICATION, fmt __VA_OPT__(,) __VA_ARGS__)
-#define LOG_INFO(fmt, ...)  SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, fmt __VA_OPT__(,) __VA_ARGS__)
-#define LOG_DEBUG(fmt, ...) SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, fmt __VA_OPT__(,) __VA_ARGS__)
-#define LOG_ERROR(fmt, ...) SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, fmt __VA_OPT__(,) __VA_ARGS__)
-#define LOG_CRIT(fmt, ...) SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, fmt __VA_OPT__(,) __VA_ARGS__)
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,50 @@
-#include "Game.hpp"
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_opengl.h>
+#include <iostream>
 
-#if defined RMLUI_PLATFORM_WIN32
-	#include <RmlUi_Include_Windows.h>
-int APIENTRY WinMain(HINSTANCE /*instance_handle*/, HINSTANCE /*previous_instance_handle*/, char* /*command_line*/, int /*command_show*/)
-#else
-int main(int /*argc*/, char** /*argv*/)
-#endif
+int main()
 {
-    if(!Game::Initialize())
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("NeoCraft (A Minecraft clone)", 600, 400,
+        SDL_WINDOW_RESIZABLE|SDL_WINDOW_OPENGL);
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+    if (!glContext) {
+        std::cerr << "SDL_GL_CreateContext failed: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
         return -1;
-    return Game::Run();
-}
+    }
+
+    int interval;
+    if(SDL_GL_GetSwapInterval(&interval))
+    {
+        std::cout << "Swap interval: " <<  interval << std::endl;
+    }
+
+    glClearColor(0.1333f, 0.1333f, 0.2667f, 1.0f);
+
+    bool running = true;
+
+    while(running)
+    {
+        SDL_Event event;
+        while(SDL_PollEvent(&event))
+        {
+            if(event.type == SDL_EVENT_QUIT)
+                running = false;
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        SDL_GL_SwapWindow(window);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+};
